0d3dfdb0e56f7f943263ea1951c6ae6a56583591
Weird Wolf <145259623+akhilesh3425@users.noreply.github.com>
2025-06-18 17:36:28 +0530

    Revert "05  authentication socials"

--- drizzle/0000_cynical_marrow.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE "user" (
-	"id" INT IDENTITY(1,1) PRIMARY KEY,
-	"name" varchar(255) NOT NULL,
-	"email" varchar(255) NOT NULL,
-	"emailVerified" boolean DEFAULT false NOT NULL,
-	CONSTRAINT "user_email_unique" UNIQUE("email")
-);
