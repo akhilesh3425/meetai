3c7428a7708aa9202c9b1b3708eec2fe35e0f401
akhilesh3425 <akhileshbutola01@gmail.com>
2025-07-03 00:52:14 +0530

    11-agent-form

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,6 +1,6 @@
-import { baseProcedure, createTRPCRouter } from "../init";
-
 import { agentsRouter } from "@/modules/agents/server/procedures";
+
+import { createTRPCRouter } from "../init";
 
 export const appRouter = createTRPCRouter({
   agents: agentsRouter,
